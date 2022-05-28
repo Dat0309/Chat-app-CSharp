51197b68bb232f4ff18b83fe680baf6915f8c50d
Dat0309 <anhtheboy011@gmail.com>
2022-05-28 10:08:39 +0700

    Update

---
+++ app-chat/app-chat/LoginForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace app_chat
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void gunaTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            if(txtAccount.Text == "" && txtPass.Text == "")
+            {
+                new ChatForm().Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Sai tên đăng nhập hoặc mật khẩu !");
+                txtAccount.Clear();
+                txtPass.Clear();
+                txtAccount.Focus();
+            }
+        }
+
+        private void gunaAdvenceButton2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void lbSignUp_Click(object sender, EventArgs e)
+        {
+            new SignUp().Show();
+            this.Hide();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ app-chat/app-chat/SignUp.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace app_chat
+{
+    partial class SignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SignUp));
+            this.lbSignIn = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gunaAdvenceButton2 = new Guna.UI.WinForms.GunaAdvenceButton();
+            this.SignUpBtn = new Guna.UI.WinForms.GunaAdvenceButton();
+            this.gunaCirclePictureBox1 = new Guna.UI.WinForms.GunaCirclePictureBox();
+            this.txtName = new Guna.UI.WinForms.GunaTextBox();
+            this.txtAccount = new Guna.UI.WinForms.GunaTextBox();
+            this.txtPass = new Guna.UI.WinForms.GunaTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gunaCirclePictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbSignIn
+            // 
+            this.lbSignIn.AutoSize = true;
+            this.lbSignIn.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSignIn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(152)))), ((int)(((byte)(255)))));
+            this.lbSignIn.Location = new System.Drawing.Point(215, 520);
+            this.lbSignIn.Name = "lbSignIn";
+            this.lbSignIn.Size = new System.Drawing.Size(87, 18);
+            this.lbSignIn.TabIndex = 24;
+            this.lbSignIn.Text = "Đăng nhập";
+            this.lbSignIn.Click += new System.EventHandler(this.lbSignIn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(23, 520);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 18);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Bạn đã có tài khoản ?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // gunaAdvenceButton2
+            // 
+            this.gunaAdvenceButton2.AnimationHoverSpeed = 0.07F;
+            this.gunaAdvenceButton2.AnimationSpeed = 0.03F;
+            this.gunaAdvenceButton2.BackColor = System.Drawing.Color.Transparent;
+            this.gunaAdvenceButton2.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(93)))), ((int)(((byte)(93)))));
+            this.gunaAdvenceButton2.BorderColor = System.Drawing.Color.Black;
+            this.gunaAdvenceButton2.CheckedBaseColor = System.Drawing.Color.Gray;
+            this.gunaAdvenceButton2.CheckedBorderColor = System.Drawing.Color.Black;
+            this.gunaAdvenceButton2.CheckedForeColor = System.Drawing.Color.White;
+            this.gunaAdvenceButton2.CheckedImage = ((System.Drawing.Image)(resources.GetObject("gunaAdvenceButton2.CheckedImage")));
+            this.gunaAdvenceButton2.CheckedLineColor = System.Drawing.Color.DimGray;
+            this.gunaAdvenceButton2.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.gunaAdvenceButton2.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaAdvenceButton2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaAdvenceButton2.ForeColor = System.Drawing.Color.White;
+            this.gunaAdvenceButton2.Image = null;
+            this.gunaAdvenceButton2.ImageSize = new System.Drawing.Size(20, 20);
+            this.gunaAdvenceButton2.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(93)))), ((int)(((byte)(93)))));
+            this.gunaAdvenceButton2.Location = new System.Drawing.Point(332, 12);
+            this.gunaAdvenceButton2.Name = "gunaAdvenceButton2";
+            this.gunaAdvenceButton2.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(93)))), ((int)(((byte)(93)))));
+            this.gunaAdvenceButton2.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.gunaAdvenceButton2.OnHoverForeColor = System.Drawing.Color.White;
+            this.gunaAdvenceButton2.OnHoverImage = null;
+            this.gunaAdvenceButton2.OnHoverLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(93)))), ((int)(((byte)(93)))));
+            this.gunaAdvenceButton2.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaAdvenceButton2.Radius = 5;
+            this.gunaAdvenceButton2.Size = new System.Drawing.Size(30, 30);
+            this.gunaAdvenceButton2.TabIndex = 20;
+            this.gunaAdvenceButton2.Text = "X";
+            this.gunaAdvenceButton2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.gunaAdvenceButton2.Click += new System.EventHandler(this.gunaAdvenceButton2_Click);
+            // 
+            // SignUpBtn
+            // 
+            this.SignUpBtn.AnimationHoverSpeed = 0.07F;
+            this.SignUpBtn.AnimationSpeed = 0.03F;
+            this.SignUpBtn.BackColor = System.Drawing.Color.Transparent;
+            this.SignUpBtn.BaseColor = System.Drawing.Color.White;
+            this.SignUpBtn.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(152)))), ((int)(((byte)(255)))));
+            this.SignUpBtn.BorderSize = 2;
+            this.SignUpBtn.CheckedBaseColor = System.Drawing.Color.Gray;
+            this.SignUpBtn.CheckedBorderColor = System.Drawing.Color.Black;
+            this.SignUpBtn.CheckedForeColor = System.Drawing.Color.White;
+            this.SignUpBtn.CheckedImage = ((System.Drawing.Image)(resources.GetObject("SignUpBtn.CheckedImage")));
+            this.SignUpBtn.CheckedLineColor = System.Drawing.Color.DimGray;
+            this.SignUpBtn.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.SignUpBtn.FocusedColor = System.Drawing.Color.Empty;
+            this.SignUpBtn.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SignUpBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(152)))), ((int)(((byte)(255)))));
+            this.SignUpBtn.Image = null;
+            this.SignUpBtn.ImageSize = new System.Drawing.Size(20, 20);
+            this.SignUpBtn.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(152)))), ((int)(((byte)(255)))));
+            this.SignUpBtn.Location = new System.Drawing.Point(78, 417);
+            this.SignUpBtn.Name = "SignUpBtn";
+            this.SignUpBtn.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(152)))), ((int)(((byte)(255)))));
+            this.SignUpBtn.OnHoverBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(152)))), ((int)(((byte)(255)))));
+            this.SignUpBtn.OnHoverForeColor = System.Drawing.Color.White;
+            this.SignUpBtn.OnHoverImage = null;
+            this.SignUpBtn.OnHoverLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(152)))), ((int)(((byte)(255)))));
+            this.SignUpBtn.OnPressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(152)))), ((int)(((byte)(255)))));
+            this.SignUpBtn.Radius = 15;
+            this.SignUpBtn.Size = new System.Drawing.Size(202, 53);
+            this.SignUpBtn.TabIndex = 19;
+            this.SignUpBtn.Text = "Đăng ký";
+            this.SignUpBtn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // gunaCirclePictureBox1
+            // 
+            this.gunaCirclePictureBox1.BaseColor = System.Drawing.Color.White;
+            this.gunaCirclePictureBox1.Image = global::app_chat.Properties.Resources.fb_messenger1;
+            this.gunaCirclePictureBox1.Location = new System.Drawing.Point(145, 34);
+            this.gunaCirclePictureBox1.Name = "gunaCirclePictureBox1";
+            this.gunaCirclePictureBox1.Size = new System.Drawing.Size(80, 80);
+            this.gunaCirclePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.gunaCirclePictureBox1.TabIndex = 18;
+            this.gunaCirclePictureBox1.TabStop = false;
+            this.gunaCirclePictureBox1.UseTransfarantBackground = false;
+            this.gunaCirclePictureBox1.Click += new System.EventHandler(this.gunaCirclePictureBox1_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.Color.Transparent;
+            this.txtName.BaseColor = System.Drawing.Color.White;
+            this.txtName.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(182)))), ((int)(((byte)(182)))));
+            this.txtName.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtName.FocusedBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(182)))), ((int)(((byte)(182)))));
+            this.txtName.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(182)))), ((int)(((byte)(182)))));
+            this.txtName.FocusedForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(182)))), ((int)(((byte)(182)))));
+            this.txtName.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(182)))), ((int)(((byte)(182)))));
+            this.txtName.Location = new System.Drawing.Point(26, 181);
+            this.txtName.Name = "txtName";
+            this.txtName.PasswordChar = '\0';
+            this.txtName.Radius = 10;
+            this.txtName.SelectedText = "";
+            this.txtName.Size = new System.Drawing.Size(312, 55);
+            this.txtName.TabIndex = 25;
+            this.txtName.Text = "   Tên";
+            // 
+            // txtAccount
+            // 
+            this.txtAccount.BackColor = System.Drawing.Color.Transparent;
+            this.txtAccount.BaseColor = System.Drawing.Color.White;
+            this.txtAccount.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(182)))), ((int)(((byte)(182)))));
+            this.txtAccount.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtAccount.FocusedBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(182)))), ((int)(((byte)(182)))));
+            this.txtAccount.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(182)))), ((int)(((byte)(182)))));
+            this.txtAccount.FocusedForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(182)))), ((int)(((byte)(182)))));
+            this.txtAccount.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAccount.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(182)))), ((int)(((byte)(182)))));
+            this.txtAccount.Location = new System.Drawing.Point(26, 251);
+            this.txtAccount.Name = "txtAccount";
+            this.txtAccount.PasswordChar = '\0';
+            this.txtAccount.Radius = 10;
+            this.txtAccount.SelectedText = "";
+            this.txtAccount.Size = new System.Drawing.Size(312, 55);
+            this.txtAccount.TabIndex = 26;
+            this.txtAccount.Text = "   Tài khoản";
+            // 
+            // txtPass
+            // 
+            this.txtPass.BackColor = System.Drawing.Color.Transparent;
+            this.txtPass.BaseColor = System.Drawing.Color.White;
+            this.txtPass.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(182)))), ((int)(((byte)(182)))));
+            this.txtPass.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtPass.FocusedBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(182)))), ((int)(((byte)(182)))));
+            this.txtPass.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(182)))), ((int)(((byte)(182)))));
+            this.txtPass.FocusedForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(182)))), ((int)(((byte)(182)))));
+            this.txtPass.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(182)))), ((int)(((byte)(182)))));
+            this.txtPass.Location = new System.Drawing.Point(26, 322);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '\0';
+            this.txtPass.Radius = 10;
+            this.txtPass.SelectedText = "";
+            this.txtPass.Size = new System.Drawing.Size(312, 55);
+            this.txtPass.TabIndex = 27;
+            this.txtPass.Text = "   Mật khẩu";
+            // 
+            // SignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(374, 601);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtAccount);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lbSignIn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gunaAdvenceButton2);
+            this.Controls.Add(this.SignUpBtn);
+            this.Controls.Add(this.gunaCirclePictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SignUp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SignUp";
+            ((System.ComponentModel.ISupportInitialize)(this.gunaCirclePictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbSignIn;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI.WinForms.GunaAdvenceButton gunaAdvenceButton2;
+        private Guna.UI.WinForms.GunaAdvenceButton SignUpBtn;
+        private Guna.UI.WinForms.GunaCirclePictureBox gunaCirclePictureBox1;
+        private Guna.UI.WinForms.GunaTextBox txtName;
+        private Guna.UI.WinForms.GunaTextBox txtAccount;
+        private Guna.UI.WinForms.GunaTextBox txtPass;
+    }
+}
